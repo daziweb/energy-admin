df77f9aae48fbf280685d48a997219faed61becd
Dagan <1024933801@qq.com>
2019-09-26 19:53:20 +0800

    登录相关操作
    (cherry picked from commit 12bb67206d5d47a100856425f95f28bb88f2ee80)

--- config/base.config.js
+++ config/base.config.js
@@ -1,12 +1,13 @@
-const env = process.argv[2].split('=')[1];
+const env = process.argv[2].split('=')[1]
 
 module.exports = {
-  base: {
-    name: 'energy',
-    version: '1.0.0',
-    author: 'QiuDaGang',
-    email: '1024933801@qq.com'
-  },
-  server:
-    env === 'production' ? require('./prod.config') : require('./dev.config')
-};
+	base: {
+		name: 'energy',
+		version: '1.0.0',
+		author: 'QiuDaGang',
+		email: '1024933801@qq.com'
+	},
+	token: 'task-admin-token',
+	server:
+		env === 'production' ? require('./prod.config') : require('./dev.config')
+}
--- controllers/task.js
+++ controllers/task.js
@@ -1,31 +1,100 @@
 // 任务清单 controller
-const TaskUser = require('../models/taskuser');
-const TaskList = require('../models/tasklist');
-const moment = require('moment');
+const TaskUser = require('../models/taskuser')
+const TaskList = require('../models/tasklist')
+const moment = require('moment')
+const utility = require('utility')
+const config = require('../config/base.config')
+
+// 生成token
+const getUserInfo = async ctx => {
+	const { username } = ctx.query
+
+	let user = await TaskUser.findOne({
+		where: {
+			username
+		}
+	})
+
+	try {
+		ctx.body = {
+			success: true,
+			message: '查询成功',
+			data: user
+		}
+	} catch {
+		ctx.body = {
+			success: false,
+			message: '查询失败'
+		}
+	}
+}
+
+// 用户登录
+const login = async ctx => {
+	const { username } = ctx.query
+
+	let user = await TaskUser.findOne({
+		where: {
+			username
+		}
+	})
+
+	if (!user) {
+		ctx.body = {
+			success: false,
+			message: '用户不存在'
+		}
+
+		return
+	}
+
+	let token = await utility.md5(username + config.token + new Date().getTime())
+
+	let updatetoken = await TaskUser.update(
+		{ token },
+		{
+			where: {
+				username
+			}
+		}
+	)
+
+	if (updatetoken) {
+		ctx.body = {
+			success: true,
+			message: '登录成功'
+		}
+	} else {
+		ctx.body = {
+			success: false,
+			message: '查询失败, 用户不存在'
+		}
+	}
+}
 
 // 查询所有任务清单
 const findTaskListAll = async ctx => {
-  let list = await TaskList.findAll({
-    order: [['id', 'DESC']]
-  });
+	let list = await TaskList.findAll({
+		order: [['id', 'DESC']]
+	})
 
-  try {
-    ctx.body = {
-      success: true,
-      message: '查询成功',
-      data: list
-    };
-  } catch {
-    ctx.body = {
-      success: false,
-      message: '查询失败'
-    };
-  }
-};
+	try {
+		ctx.body = {
+			success: true,
+			message: '查询成功',
+			data: list
+		}
+	} catch {
+		ctx.body = {
+			success: false,
+			message: '查询失败'
+		}
+	}
+}
 
 // 创建任务
 const createTask = async ctx => {
-  const { userid, content } = ctx.request.body;
-};
+	const { userid, content } = ctx.request.body
+}
 
-module.exports = { findTaskListAll, createTask };
+module.exports = { findTaskListAll, createTask, login, getUserInfo }
