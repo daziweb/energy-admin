5d3967787221246e2aa549c955c2718850f68e5f
Dagan <1024933801@qq.com>
2019-09-29 17:08:24 +0800

    完成任务操作

--- routes/task.js
+++ routes/task.js
@@ -9,5 +9,6 @@ router.get('/list', controller.findTaskListAll)
 router.get('/user/login', controller.login)
 router.get('/user/info', controller.getUserInfo)
 router.post('/createtask', controller.createTask)
+router.post('/donetask', controller.doneTask)
 
 module.exports = router
